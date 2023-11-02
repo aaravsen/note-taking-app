f512b9dc52b04021f6933fcaa45bbf8bf1d70f4d
aarav sen <50616567+aaravsen@users.noreply.github.com>
2023-11-02 20:13:14 +0530

    Deleted some components

--- src/components/functional-component-with-props.jsx
+++
@@ -1,9 +0,0 @@
-const FunctionalComponentWithProps = (props) => {
-    return ( 
-        <div>
-            <h1>Hello: {props.name} from Functional Component</h1>
-        </div>
-     );
-}
- 
-export default FunctionalComponentWithProps;
--- src/components/functional-component.jsx
+++
@@ -1,9 +0,0 @@
-const FunctionalComponent = () => {
-    return ( 
-        <div>
-            <h1>Hello from Functional Component</h1>
-        </div>
-     );
-}
- 
-export default FunctionalComponent;
